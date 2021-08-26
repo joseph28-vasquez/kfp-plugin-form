86c0fe7aa969fed8a7681fb55e791777cddbb668
Joseph <joseph28-vasquez@hotmail.com>
2021-08-26 12:47:55 -0500

    Se agrega el shortcode para mostrar el formulario

---
+++ kfp-plugin-form.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Plugin Name: KFP Plugin Form
+ * Plugin URI: https://developker.com
+ * Description: Formulario personalizado usando el shortcode [kfp_aspirante_form]
+ * Version: 1.0
+ * Author: Joseph Vasquez
+ * Author URI: https://developker.com
+ * License: GPL2
+ */
+
+// Define el shortcode y lo asocia a una función
+add_shortcode('kfp_aspirante_form', 'Kfp_Aspirante_form');
+ 
+/** 
+ * Define la función que ejecutará el shortcode
+ * De momento sólo pinta un formulario que no hace nada
+ * 
+ * @return string
+ */
+function Kfp_Aspirante_form() 
+{
+    // Esta función de PHP activa el almacenamiento en búfer de salida (output buffer)
+    // Cuando termine el formulario lo imprime con la función ob_get_clean
+    ob_start();
+    ?>
+    <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante
+class="cuestionario"">
+        <div class="form-input">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="nombre" required>
+        </div>
+        <div class="form-input">
+            <label for='correo'>Correo</label>
+            <input type="email" name="correo" id="correo" required>
+        </div>
+        <div class="form-input">
+            <label for="nivel_html">¿Cuál es tu nivel de HTML?</label>
+            <input type="radio" name="nivel_html" value="1" required> Nada
+            <br><input type="radio" name="nivel_html" value="2" required> Estoy 
+                aprendiendo
+            <br><input type="radio" name="nivel_html" value="3" required> Tengo 
+                experiencia
+            <br><input type="radio" name="nivel_html" value="4" required> Lo 
+                domino al dedillo
+        </div>
+        <div class="form-input">
+            <label for="nivel_css">¿Cuál es tu nivel de CSS?</label>
+            <input type="radio" name="nivel_css" value="1" required> Nada
+            <br><input type="radio" name="nivel_css" value="2" required> Estoy 
+                aprendiendo
+            <br><input type="radio" name="nivel_css" value="3" required> Tengo 
+                experiencia
+            <br><input type="radio" name="nivel_css" value="4" required> Lo 
+                domino al dedillo
+        </div>
+        <div class="form-input">
+            <label for="nivel_js">¿Cuál es tu nivel de JavaScript?</label>
+            <input type="radio" name="nivel_js" value="1" required> Nada
+            <br><input type="radio" name="nivel_js" value="2" required> Estoy 
+                aprendiendo
+            <br><input type="radio" name="nivel_js" value="3" required> Tengo 
+                experiencia
+            <br><input type="radio" name="nivel_js" value="4" required> Lo domino 
+al dedillo
+        </div>
+        <div class="form-input">
+            <label for="aceptacion">La información facilitada se tratará 
+            con respeto y admiración.</label>
+            <input type="checkbox" id="aceptacion" name="aceptacion"
+value="1" required> Entiendo y acepto las condiciones
+        </div>
+        <div class="form-input">
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
+    <?php
+     
+    // Devuelve el contenido del buffer de salida
+    return ob_get_clean();
+}
